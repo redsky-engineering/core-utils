570f288f59d495c5c40d1b4b2769ac003533a477
Tanner Burton <tburt713@gmail.com>
2024-10-10 12:52:34 -0600

    feat: remove jest and use mocha so that we can use tsx instead of ts-node

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	testMatch: ['<rootDir>/tests/**/*.(spec|test).[jt]s?(x)'], // Look inside the tests folder
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-	transform: {
-		'^.+\\.tsx?$': 'ts-jest'
-	}
-};
--- tests/NumberUtils.spec.ts
+++ tests/NumberUtils.spec.ts
@@ -1,44 +1,45 @@
 import { NumberUtils } from '../src/';
+import {expect} from 'chai';
 
 describe('NumberUtils', () => {
 	it('deg2Rad should convert degrees to radians', () => {
-		expect(NumberUtils.deg2Rad(0)).toBe(0);
-		expect(NumberUtils.deg2Rad(180)).toBeCloseTo(Math.PI);
-		expect(NumberUtils.deg2Rad(360)).toBeCloseTo(2 * Math.PI);
+		expect(NumberUtils.deg2Rad(0)).to.equal(0);
+		expect(NumberUtils.deg2Rad(180)).to.be.closeTo(Math.PI, 0.001);
+		expect(NumberUtils.deg2Rad(360)).to.be.closeTo(2 * Math.PI,0.001);
 	});
 
 	it('rad2Deg should convert radians to degrees', () => {
-		expect(NumberUtils.rad2Deg(0)).toBe(0);
-		expect(NumberUtils.rad2Deg(Math.PI)).toBe(180);
-		expect(NumberUtils.rad2Deg(2 * Math.PI)).toBe(360);
+		expect(NumberUtils.rad2Deg(0)).to.equal(0);
+		expect(NumberUtils.rad2Deg(Math.PI)).to.equal(180);
+		expect(NumberUtils.rad2Deg(2 * Math.PI)).to.equal(360);
 	});
 
 	it('dollarsToCents should convert dollars to cents', () => {
-		expect(NumberUtils.dollarsToCents(1.0)).toBe(100);
-		expect(NumberUtils.dollarsToCents(0.99)).toBe(99);
-		expect(NumberUtils.dollarsToCents(0.01)).toBe(1);
+		expect(NumberUtils.dollarsToCents(1.0)).to.equal(100);
+		expect(NumberUtils.dollarsToCents(0.99)).to.equal(99);
+		expect(NumberUtils.dollarsToCents(0.01)).to.equal(1);
 	});
 
 	it('centsToDollars should convert cents to dollars', () => {
-		expect(NumberUtils.centsToDollars(100)).toBe(1.0);
-		expect(NumberUtils.centsToDollars(99)).toBe(0.99);
-		expect(NumberUtils.centsToDollars(1)).toBe(0.01);
+		expect(NumberUtils.centsToDollars(100)).to.equal(1.0);
+		expect(NumberUtils.centsToDollars(99)).to.equal(0.99);
+		expect(NumberUtils.centsToDollars(1)).to.equal(0.01);
 	});
 
 	it('round should round a number to the nearest significance', () => {
-		expect(NumberUtils.round(123, 5)).toBe(125);
-		expect(NumberUtils.round(123, 10)).toBe(120);
-		expect(NumberUtils.round(0, 10)).toBe(0);
-		expect(NumberUtils.round(-123, 5)).toBe(-125);
-		expect(NumberUtils.round(-123, 10)).toBe(-120);
+		expect(NumberUtils.round(123, 5)).to.equal(125);
+		expect(NumberUtils.round(123, 10)).to.equal(120);
+		expect(NumberUtils.round(0, 10)).to.equal(0);
+		expect(NumberUtils.round(-123, 5)).to.equal(-125);
+		expect(NumberUtils.round(-123, 10)).to.equal(-120);
 	});
 
 	it('randomNumberInRange should return a random number in the given range', () => {
 		const maxLimit = 10;
 		for (let i = 0; i < 100; i++) {
 			const randomNum = NumberUtils.randomNumberInRange(maxLimit);
-			expect(randomNum).toBeGreaterThanOrEqual(0);
-			expect(randomNum).toBeLessThan(maxLimit);
+			expect(randomNum).to.be.gte(0);
+			expect(randomNum).to.be.lt(maxLimit);
 		}
 	});
 
@@ -47,8 +48,8 @@ describe('NumberUtils', () => {
 		const max = 10;
 		for (let i = 0; i < 100; i++) {
 			const randomNum = NumberUtils.randomInclusiveInRange(min, max);
-			expect(randomNum).toBeGreaterThanOrEqual(min);
-			expect(randomNum).toBeLessThanOrEqual(max);
+			expect(randomNum).to.be.gte(min);
+			expect(randomNum).to.be.lte(max);
 		}
 	});
 });
